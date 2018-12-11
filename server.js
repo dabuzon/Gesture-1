f013a0b1c4f5902539c3cf80457723d807252229
Darin Buzon <dabuzon@Darins-iMac.local>
2018-12-11 00:52:00 -0800

    Plan B boys

---
+++ server.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+
+server.listen(process.env.PORT || 3000);
+console.log('Server running');
+
+app.get('/', function (req, res) {
+    res.sendFile('/public/index.html');
+});
