de9e2e955452a5f90a657d14fbc4a5bcd0e35126
Darin Buzon <dabuzon@Darins-iMac.local>
2018-12-11 01:41:28 -0800

    Configure sections for connections

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-var app = require('express')();
+var app = require('express');
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
+connections = [];
 
 server.listen(process.env.PORT || 3000);
 console.log('Server running');
 
-app.get('/', function (req, res) {
-    res.sendFile('/public/index.html');
+app.use(express.static('public'));
+
+io.on('connection', function (socket) {
+    connections.push(socket);
+    console.log('Connected: %s sockets', connections.length);
+
+    socket.on('send message', function(data) {
+        io.sockets.emit('new message', {msg: data});
+    });
 });
